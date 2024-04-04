41a82f148fcb5e21f0fd62960f45539eca9e5a29
BrageDrage909 <bfu01.bb@gmail.com>
2024-04-04 15:33:46 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const staticPath = path.join(__dirname, 'public');
+const staticPath = path.join(__dirname);
 app.use(express.static(staticPath));
 
 app.get('/', (req, res) => {
