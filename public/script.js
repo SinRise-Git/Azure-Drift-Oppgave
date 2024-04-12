3148bd12dbb81fd335134e2635185d3f4e3df3a6
BrageDrage909 <bfu01.bb@gmail.com>
2024-04-12 08:54:57 +0200

    update

--- public/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('getRequest').addEventListener('click', async function(e) {
-    response = await fetch('/api')
-    responseJson = await response.json()
-    console.log(responseJson)
-})
